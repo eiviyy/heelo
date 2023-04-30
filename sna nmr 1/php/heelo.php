59d826f3901f2b8a5955c8266afc340e7b03ffcb
eiviyy <125943155+eiviyy@users.noreply.github.com>
2023-05-01 00:03:48 +0700

    Create heelo.php

---
+++ sna nmr 1/php/heelo.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "172.17.0.2:3306";
+$username = "root";
+$password = "lalalla";
+$dbname = "Redlock";
+
+$dbs = new mysqli($servername, $username, $password, $dbname);
+
+if($dbs->connect_error){
+    die("Connection failed: " . $dbs->connect_error);
+}
+
+$sql = "SELECT COUNT(*) FROM users";
+$result = $dbs->query($sql);
+
+while($row = $result->fetch_assoc()){
+    echo $row;
+}
+
+$dbs->close();
+?>
